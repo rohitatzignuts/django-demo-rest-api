922f400720dcb0f6e17dd346d2f2e41404538258
rohitatzignuts <rohitv@zignuts.com>
2024-10-08 17:55:18 +0530

    format_suffix_patterns

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("snippets/", include("snippets.urls")),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
